aa913967b2bb663af989cbe61569f7a84ae91154
Francisco Lopez <82164792+franciscols2011@users.noreply.github.com>
2023-12-19 01:15:27 +0000

    Landing-page-React

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function Footer() {
+  return (
+    <nav className="navbar fixed-bottom bg-dark ">
+      <div className="container-fluid d-flex justify-content-center">
+        <a className="navbar-brand text-light ">
+          Copyright © Your Website 2023
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Footer;
